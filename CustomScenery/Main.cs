e0a65a8d98dee1d75f775e14c6f85efbb4c7eca5
shyguySGW <shyguy123@cox.net>
2016-05-12 11:12:53 -0700

    v1.1

---
+++ CustomScenery/Main.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Custom_Scenery
+{
+    public class Main : IMod
+    {
+        private GameObject _go;
+
+        public void onEnabled()
+        {
+            _go = new GameObject();
+
+            _go.AddComponent<SceneryLoader>();
+
+            _go.GetComponent<SceneryLoader>().Path = Path;
+
+            _go.GetComponent<SceneryLoader>().Identifier = Identifier;
+
+            _go.GetComponent<SceneryLoader>().LoadScenery();
+        }
+
+        public void onDisabled()
+        {
+            _go.GetComponent<SceneryLoader>().UnloadScenery();
+
+            Object.Destroy(_go);
+        }
+
+        public string Name { get { return "shyguy's Building Set 2"; } }
+        public string Description { get { return "Clapboard walls, slanted roofs, posts, awnings, signs, and more."; } }
+        public string Path { get; set; }
+        public string Identifier { get; set; }
+    }
+}
